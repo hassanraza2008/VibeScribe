51cdc337ada1f7f676fdb6879694d237845193d1
Hassan Raza <mrismart70@gmail.com>
2024-12-19 01:21:07 +0500

    adding upload functionality and states variables of it

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
-export default function HomePage() {
+export default function HomePage(props) {
+  const { setAudioStream, setFile } = props;
+    
   return (
     <main className='flex-1 p-4 flex flex-col gap-3 sm:gap-4 md:gap-5 text-center justify-center pb-20'>
         <h1 className='font-semibold text-5xl sm:text-6xl md:text-7xl'>Vibe<span className='text-teal-500 bold'>Scribe</span></h1>
@@ -11,7 +13,10 @@ export default function HomePage() {
             <p className='text-teal-400'>Record</p>
             <i className="fa-solid fa-microphone"></i>
         </button>
-        <p className='text-base'>Or <label className='text-teal-300 cursor-pointer hover:text-teal-500 duration-200'>upload <input className='hidden' type='file' accept='.mp3, .wav' /></label> a mp3 file</p>
+        <p className='text-base'>Or <label className='text-teal-300 cursor-pointer hover:text-teal-500 duration-200'>upload <input onChange={(e) => {
+            const tempFile = e.target.files[0];
+            setFile(tempFile);
+        }} className='hidden' type='file' accept='.mp3, .wav' /></label> a mp3 file</p>
         <p className='italic text-slate-500'>Free now free forever</p>
     </main>
   )

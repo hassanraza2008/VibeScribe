e9e3e20597489f1748d80380d8f6c047f99e85d5
Hassan Raza <mrismart70@gmail.com>
2024-12-19 00:35:58 +0500

    setting up barebones App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,19 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      
+    <div className='flex flex-col max-w-[1000px] mx-auto w-full'>
+      <section className='min-h-screen flex flex-col'>
+        <header className='flex items-center p-4 justify-between gap-4'>
+          <h1 className=''>Vibe<span className='text-teal-500'>Scribe</span></h1>
+          <button className='flex items-center gap-2'>
+            <p>New</p>
+            <i className="fa-solid fa-plus"></i>
+          </button>
+        </header>
+        <main className='flex-1 p-4 flex flex-col justify-center'></main>
+      </section>
+      <h1 className=''></h1>
+      <footer></footer>
     </div>
   )
 }

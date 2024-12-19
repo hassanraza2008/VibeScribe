b5063425154fcb8dbaa195edff9d0dddf57f30bd
Hassan Raza <mrismart70@gmail.com>
2024-12-19 14:49:39 +0500

    Updated App component and added new Information component
    - App.jsx:
      - Imported Information and Transcribing components.
      - Added state variables output and loading.
      - Implemented conditional rendering logic to display Information, Transcribing, FileDisplay, or HomePage based on state.
    - Created Information.jsx:
      - Added new Information component with a basic structure.

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import { useState, useEffect } from 'react'
 import HomePage from './components/Homepage'
 import Header from './components/Header'
 import FileDisplay from './components/FileDisplay';
+import Information from './components/Information';
+import Transcribing from './components/Transcribing';
 
 function App() {
   const [file, setFile] = useState(null);
   const [audioStream, setAudioStream] = useState(null);
-  
+  const [output, setOutput] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   const isAudioAvailable = file || audioStream;
 
   function handleAudioReset() {
@@ -21,7 +25,11 @@ function App() {
     <div className='flex flex-col max-w-[1000px] mx-auto w-full'>
       <section className='min-h-screen flex flex-col'>
         <Header/>
-        {isAudioAvailable ? (
+        {output ? (
+          <Information/>
+        ) : loading ? (
+          <Transcribing/>
+        ) : isAudioAvailable ? (
           <FileDisplay handleAudioReset={handleAudioReset} file={file} audioStream={setAudioStream} />  
         ) : (
           <HomePage setFile={setFile} setAudioStream={setAudioStream}/>

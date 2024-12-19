68b1de016bc440ae766e1bf431eea74df9518476
Hassan Raza <mrismart70@gmail.com>
2024-12-19 12:44:15 +0500

    Added audio recording functionality to HomePage component

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 
 export default function HomePage(props) {
   const { setAudioStream, setFile } = props;
-    
+  
+  const [recordingStatus, setRecordingStatus] = useState('inactive');
+  const [audioChunks, setAudioChunks] = useState([]);
+  const [duration, setDuration] = useState(0);
+
+  const mediaRecorder = useRef(null);
+
+  const mimeType = 'audio/webm';
+
+  async function startRecording() {
+    let tempStream;
+
+    console.log('Start Recording');
+
+    try {
+      const streamData = navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: false
+      });
+      tempStream = streamData;
+    } catch (err) {
+      console.log(err.message);
+      return
+    }
+
+    // create new Media Recorder instance using the stream
+    const media = new MediaRecorder(tempStream, { type: mimeType });
+    mediaRecorder.current = media;
+
+    mediaRecorder.current.start();
+    let localAudioChunks = [];
+    mediaRecorder.current.ondataavailable = (event) => {
+      if (typeof event.data === 'undefined') { return };
+      if (event.data.size === 0) { return };
+
+      localAudioChunks.push(event.data);
+    }
+    setAudioChunks(localAudioChunks);
+  }
+
   return (
     <main className='flex-1 p-4 flex flex-col gap-3 sm:gap-4 md:gap-5 text-center justify-center pb-20'>
         <h1 className='font-semibold text-5xl sm:text-6xl md:text-7xl'>Vibe<span className='text-teal-500 bold'>Scribe</span></h1>

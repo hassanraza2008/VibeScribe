f5be48c5394e84bf57a9c0929c984097ccc9963c
Hassan Raza <mrismart70@gmail.com>
2024-12-19 15:39:03 +0500

    Updated App and Information components
    - App.jsx:
      - Imported Information and Transcribing components.
      - Added state variables output and loading.
      - Set output state to true for testing purposes.
      - Implemented conditional rendering logic to display Information, Transcribing, FileDisplay, or HomePage based on state.
    - Information.jsx:
      - Added new Information component structure.
      - Implemented tab state and rendering logic for Transcription and Translation views.
    - Transcribing.jsx:
      - Updated div class on line 7 to include flex-1, text-center, pb-24, and p-4 for better layout and styling.
      - Adjusted div class on line 12 to m-w-[400px] for consistent layout.
    - index.css:
      - Added loading animations and styles from line 24 to 50.
      - Defined .loading class with width animation.
      - Included specific animation delays for loading1 and loading2.
      - Created @keyframes width for dynamic width changes.

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Transcribing from './components/Transcribing';
 function App() {
   const [file, setFile] = useState(null);
   const [audioStream, setAudioStream] = useState(null);
-  const [output, setOutput] = useState(null);
+  const [output, setOutput] = useState(true);
   const [loading, setLoading] = useState(true);
 
   const isAudioAvailable = file || audioStream;
--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function Information() {
+  const [tab, setTab] = useState("transcription")
+
   return (
-    <div></div>
-  )
+  <main className="flex-1 p-4 flex flex-col gap-3 sm:gap-4 text-center justify-center pb-20 max-w-prose w-full mx-auto">
+    <h1 className="font-semibold text-4xl sm:text-5xl md:text-6xl whitespace-nowrap">
+      Your <span className="text-teal-500 bold">Transcription</span>
+    </h1>
+
+    <div className="grid grid-cols-2 mx-auto bg-white shadow rounded-full overflow-hidden items-center">
+      <button onClick={() => setTab("transcription")} className={"px-4 duration-200 py-2 font-medium " + (tab === "transcription" ? ' bg-teal-500 text-white' : ' text-teal-400')}>Transcription</button>
+      <button onClick={() => setTab("translation")} className={"px-4 duration-200 py-2 font-medium " + (tab === "translation" ? ' bg-teal-500 text-white' : ' text-teal-400')}>Translation</button>
+    </div>
+  </main>
+  );
 }

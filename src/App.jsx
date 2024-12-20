00dc171994cdbe986abcf8296b5eeb6c70289d0b
Hassan Raza <mrismart70@gmail.com>
2024-12-21 00:16:05 +0500

    feat: integrate audio processing with web workers and add new utilities
    - Implemented state management for file, audioStream, output, downloading, loading, and finished in App.jsx.
    - Set up a web worker for handling audio processing tasks and added message handling for various states (DOWNLOADING, LOADING, RESULT, INFERENCE_DONE).
    - Created presets.js to define constants for message types, loading statuses, model names, and languages.
    - Added whisper.worker.js file (currently empty) to the utils folder for future audio processing logic.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import HomePage from './components/Homepage'
 import Header from './components/Header'
 import FileDisplay from './components/FileDisplay';
 import Information from './components/Information';
 import Transcribing from './components/Transcribing';
+import { MessageTypes } from './utils/presets';
 
 function App() {
   const [file, setFile] = useState(null);
   const [audioStream, setAudioStream] = useState(null);
   const [output, setOutput] = useState(null);
+  const [downloading, setDownloading] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [finished, setFinished] = useState(false);
 
   const isAudioAvailable = file || audioStream;
 
@@ -18,6 +21,61 @@ function App() {
     setAudioStream(null);
   }
 
+  const worker = useRef(null);
+
+  useEffect(() => {
+    if(!worker.current) {
+      worker.current = new Worker(new URL('./utils/whisper.worker.js', import.meta.url), {
+        type: 'module'
+      });
+    }
+
+    const onMessageReceivced = async (e) => {
+      switch (e.data.type) {
+        case 'DOWNLOADING': 
+          setDownloading(true)
+          console.log('DOWNLOADING')
+          break;
+        case 'LOADING': 
+          setLoading(true)
+          break;
+        case 'RESULT': 
+          setOutput(e.data.results)
+          break;
+        case 'INFERENCE_DONE': 
+          setFinished(true)
+          console.log('DONE')
+          break;
+      }
+    }
+
+    worker.current.addEventListener('message', onMessageReceivced)
+
+    return () => worker.current.removeEventListener('message', onMessageReceivced)
+  }, [])
+
+  async function readAudioFrom(file) {
+    const sampling_rate = 16000;
+    const audioCTX = new AudioContext({sampleRate: sampling_rate});
+    const response = await file.arrayBuffer();
+    const decoded = await audioCTX.decodeAudioData(response);
+    const audio = decoded.getChannelData(0);
+
+    return audio;
+  }
+
+  async function handleFormSubmission() {
+    if(!file && !audioStream) {return}
+
+    let audio = await readAudioFrom(file ? file : audioStream);
+    const model_name = `open-ai/whisper-tiny.en`;
+
+    worker.current.postMessage({
+      type: MessageTypes.INFERENCE_REQUEST,
+      audio,
+      model_name
+    })
+  }
   return (
     <div className='flex flex-col max-w-[1000px] mx-auto w-full'>
       <section className='min-h-screen flex flex-col'>
